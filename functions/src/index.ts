d2cdc1fc47653d51eab5415f7dfa1236b3ea57e6
Gaab <Gaab.972.gaab@gmail.com>
2025-05-19 16:42:14 +0200

    Create function createSetupIndent in functions Firebase to create stripeCustomerId and get client_secret

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,58 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import Stripe from "stripe";
+import * as dotenv from "dotenv";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+// 🔐 Initialise Firebase Admin SDK
+admin.initializeApp();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+dotenv.config();
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// eslint-disable-next-line max-len
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {});
+
+export const createSetupIntent = functions.https.onRequest(async (req, res) => {
+  try {
+    const {userId} = req.body;
+
+    if (!userId) {
+      res.status(400).send("Missing userId");
+      return;
+    }
+
+    // 🔍 On récupère l'utilisateur dans Firestore
+    const userDoc = await admin
+      .firestore()
+      .collection("users")
+      .doc(userId).get();
+    const userData = userDoc.data();
+
+    let customerId = userData?.stripeCustomerId;
+
+    // 👤 Si pas encore de customer Stripe, on le crée
+    if (!customerId) {
+      const customer = await stripe.customers.create({
+        metadata: {firebaseUID: userId},
+      });
+
+      customerId = customer.id;
+      if (!userDoc.exists) {
+        await admin.firestore().collection("users").doc(userId).set({
+          stripeCustomerId: customerId,
+        });
+      } else {
+        await userDoc.ref.update({stripeCustomerId: customerId});
+      }
+    }
+
+    // 🧾 On crée un SetupIntent
+    const setupIntent = await stripe.setupIntents.create({
+      customer: customerId,
+    });
+
+    res.status(200).json({clientSecret: setupIntent.client_secret});
+  } catch (err) {
+    console.error("Erreur createSetupIntent:", err);
+    res.status(500).send("Internal server error");
+  }
+});
