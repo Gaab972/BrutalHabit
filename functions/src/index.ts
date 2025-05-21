36a2da746511dfafc3e1639ff163ff493564abfa
Gaab <Gaab.972.gaab@gmail.com>
2025-05-21 14:40:01 +0200

    Revert "WIP storeCardDetails function and handleSaveCard"
    This reverts commit 07c74807625112d7c656c342ca7736d61a3b6058.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,58 @@
-export { createSetupIntent } from "./createSetupIntent";
-export { storeCardDetails } from "./storeCardDetails";
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import Stripe from "stripe";
+import * as dotenv from "dotenv";
 
-export const testFunction = functions.https.onRequest((req, res) => {
-  res.send("Hello from testFunction");
+// 🔐 Initialise Firebase Admin SDK
+admin.initializeApp();
+
+dotenv.config();
+
+// eslint-disable-next-line max-len
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {});
+
+export const createSetupIntent = functions.https.onRequest(async (req, res) => {
+  try {
+    const {userId} = req.body;
+
+    if (!userId) {
+      res.status(400).send("Missing userId");
+      return;
+    }
+
+    // 🔍 On récupère l'utilisateur dans Firestore
+    const userDoc = await admin
+      .firestore()
+      .collection("users")
+      .doc(userId).get();
+    const userData = userDoc.data();
+
+    let customerId = userData?.stripeCustomerId;
+
+    // 👤 Si pas encore de customer Stripe, on le crée
+    if (!customerId) {
+      const customer = await stripe.customers.create({
+        metadata: {firebaseUID: userId},
+      });
+
+      customerId = customer.id;
+      if (!userDoc.exists) {
+        await admin.firestore().collection("users").doc(userId).set({
+          stripeCustomerId: customerId,
+        });
+      } else {
+        await userDoc.ref.update({stripeCustomerId: customerId});
+      }
+    }
+
+    // 🧾 On crée un SetupIntent
+    const setupIntent = await stripe.setupIntents.create({
+      customer: customerId,
+    });
+
+    res.status(200).json({clientSecret: setupIntent.client_secret});
+  } catch (err) {
+    console.error("Erreur createSetupIntent:", err);
+    res.status(500).send("Internal server error");
+  }
 });
