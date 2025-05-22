6854a45fdf4f45853a64a963819a0bdf7d8bce2d
Gaab972 <gaab.972.gaab@gmail.com>
2025-05-22 22:28:14 +0200

    Rename component PaymentCard to PaymentCardForm

--- app/Components/PaymentCardForm.tsx
+++ app/Components/PaymentCardForm.tsx
@@ -8,11 +8,13 @@ import {
 import { database } from "@/firebaseConfig";
 import OKButton from "./OKButton";
 
-export default function PaymentCard({style} : ViewProps)
+export default function PaymentCardForm({style} : ViewProps)
 {
     const { confirmSetupIntent } = useStripe();
     const handleSaveCard = async () => {
         try {
+            console.log("01")
+
             const userId = "user_default";
             var response = await fetch("https://createsetupintent-3ir4utrw3a-ew.a.run.app", {
                 method: "POST",
@@ -24,6 +26,8 @@ export default function PaymentCard({style} : ViewProps)
 
             if (!response.ok) throw new Error("Backend Firebase createSetupIntent function error")
 
+            console.log("02");
+
             const { clientSecret } = await response.json();
             const { setupIntent, error } = await confirmSetupIntent(clientSecret, {
                 paymentMethodType: "Card",
@@ -34,20 +38,33 @@ export default function PaymentCard({style} : ViewProps)
                 return;
             }
 
+            console.log("03");
+
             const paymentMethodId = setupIntent?.paymentMethod?.id;
             if (!paymentMethodId) throw new Error("No payment method recovered");
+
+            const userRef = doc(database, "users", userId);
+            await updateDoc(userRef, {
+                paymentMethodId,
+            });
 
             response = await fetch("https://storecarddetails-3ir4utrw3a-ew.a.run.app", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                 },
+                body: JSON.stringify({ 
+                    userId: userId,
+                    paymentMethodId: paymentMethodId,
+                })
             })
+            
+            console.log("04");
 
-            const userRef = doc(database, "users", userId);
-            await updateDoc(userRef, {
-                paymentMethodId,
-            });
+            if (!response.ok) throw new Error("Backend Firebase createSetupIntent function error")
+
+            console.log("05");
+            
         } catch (err: any) {
             Alert.alert("Error", err.message || "ERROR");
         }
@@ -73,9 +90,15 @@ export default function PaymentCard({style} : ViewProps)
                 borderWidth: 1,
                 }}
             />
-            <OKButton style={{width: "40%"}} text="Save card" onPress={() => handleSaveCard}/>
+            <OKButton style={{width: "40%"}} text="Save card" onPress={onPress}/>
         </View>
     )
+
+    function onPress()
+    {
+        console.log("ON PRESS")
+        handleSaveCard();
+    }
 }
 
 const styles = StyleSheet.create({
